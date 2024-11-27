41345fc4fde09788006eef620534f0e144825cd8
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2024-11-27 01:36:34 -0300

    feat: :sparkles: creation of the history page

---
+++ frontend/src/pages/historical/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/header";
+
+const HistoricalPage = () => {
+  return (
+    <>
+      <Header />
+      <h1>Histórico de viagens</h1>
+    </>
+  );
+};
+
+export default HistoricalPage;
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/login/page";
+import HomePage from "../pages/home/page";
+import HistoricalPage from "../pages/historical/page";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/login" />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/historical" element={<HistoricalPage />} />
       </Routes>
     </BrowserRouter>
   );
